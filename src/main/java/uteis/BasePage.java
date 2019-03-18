983635bcbf4d26f0ea4911d0b9a0261a498d1d1e
gerrysousa <gerry.nett@gmail.com>
2019-03-18 17:49:24 -0300

    Add estrutura do projeto

---
+++ src/main/java/uteis/BasePage.java
@@ -0,0 +1,53 @@
+package uteis;
+
+import static uteis.DriverFactory.getDriver;
+
+import org.openqa.selenium.By;
+
+public class BasePage {
+
+public String getUrlBase(){
+		return "http://mantis.glaucia.base2.com.br/login_page.php";
+	}
+	
+	public void escrever(By by, String texto) {
+		getDriver().findElement(by).sendKeys(texto);
+	}
+
+	public String obterTexto(By by) {
+		return getDriver().findElement(by).getText();
+	}
+	
+	public void clicar(By by) {
+		getDriver().findElement(by).click();
+	}
+	
+	public void clicarTexto(String texto) {
+		clicar(By.xpath("//*[@text='"+texto+"']"));
+	}
+	
+	public void selecionarCombo(By by, String valor) {
+		getDriver().findElement(by).click();
+		clicarTexto(valor);
+	}
+	
+	public boolean isCheckMarcado(By by) {
+		return getDriver().findElement(by).getAttribute("checked").equals("true");		
+	}
+	
+		
+	public String obterTituloAlerta() {
+		return obterTexto(By.id("alertTitle"));
+	}
+	
+	public String obterMensageAlerta() {
+		return obterTexto(By.id("id/message"));
+	}
+	
+	public void esperar(int num) throws InterruptedException {
+		Thread.sleep(num);
+	}
+	
+
+	
+}
---
+++ src/main/java/uteis/DriverFactory.java
@@ -0,0 +1,43 @@
+package uteis;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DriverFactory {
+	private static WebDriver driver;
+
+   	public static WebDriver getDriver() {   		
+		if (driver == null) {
+			 createDrive();
+		}
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		
+		return driver;
+	}
+
+	private static void createDrive() {
+		try {			
+				System.setProperty("webdriver.gecko.driver", "C:\\workspace\\Ambiente\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+				System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+				
+				driver = new ChromeDriver();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	}	
+
+	public static void killDriver() 	{
+		if (driver != null) 
+		{
+			driver.quit();
+			driver = null;
+		}
+	}
+
+}
